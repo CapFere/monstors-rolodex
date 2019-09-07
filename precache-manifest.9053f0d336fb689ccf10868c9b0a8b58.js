46ddb9fa45389f4a4371c0836e14da5de63c4fbc
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-09-07 16:16:53 +0300

    Updates

---
+++ precache-manifest.9053f0d336fb689ccf10868c9b0a8b58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf5f82f6cde60f4c87f024ca396ba41",
+    "url": "/monstors-rolodex/index.html"
+  },
+  {
+    "revision": "fce05f9801f4df8c8ba1",
+    "url": "/monstors-rolodex/static/css/main.9c06ccd1.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monstors-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "fce05f9801f4df8c8ba1",
+    "url": "/monstors-rolodex/static/js/main.c0ff8fd7.chunk.js"
+  },
+  {
+    "revision": "f240ed2bb965cf08a18c",
+    "url": "/monstors-rolodex/static/js/runtime~main.7b5adb0f.js"
+  }
+]);
